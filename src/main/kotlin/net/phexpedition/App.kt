9d8b06bebaba0a5688061a5722e2294c62208f21
Timo Böwing <bjblazko@users.noreply.github.com>
2023-05-29 17:39:05 +0200

    chore: added some more HTTP headers

--- src/main/kotlin/net/phexpedition/App.kt
+++ src/main/kotlin/net/phexpedition/App.kt
@@ -12,6 +12,9 @@ class App {
 class HttpResponseFilter : ContainerResponseFilter {
     override fun filter(requestContext: ContainerRequestContext?, responseContext: ContainerResponseContext?) {
         responseContext!!.headers.add("X-Frame-Options", "SAMEORIGIN")
+        responseContext.headers.add("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
+        responseContext.headers.add("X-Content-Type-Options", "nosniff")
+        responseContext.headers.add("Content-Security-Policy", "default-src https: 'unsafe-inline'")
     }
 
 }
